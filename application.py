ac87381db1260abfe4a6f456e4f616e61ac6b93d
Kumar <Nilesh_Mishra1@Dell.com>
2020-06-09 10:35:25 +0530

    initial commit

---
+++ application.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[65]:
+
+
+import pandas as pd
+from flask import Flask, jsonify, Response
+import os
+
+
+# In[66]:
+
+
+def data_processing(dataframe,index):
+    if dataframe['server'].unique()[0] == '100.101.2.50':
+       df = dataframe.drop(['Unnamed: 0','model_name','proc_core_speed','proc_l2_cache','proc_l3_cache',"CPU1VCoreVR",'CPU2VCoreVR','CPU1MEM0123VR','CPU1MEM4567VR','CPU2MEM0123VR','CPU2MEM4567VR'],axis=1) 
+       df = df.dropna() 
+    else:
+        df = dataframe.dropna()
+        df = df.drop(['Unnamed: 0','model_name','proc_core_speed','proc_l2_cache','proc_l3_cache'],axis=1)
+    return df
+
+
+# In[67]:
+
+
+app = Flask(__name__)
+
+path = 'DataSensNew'
+
+@app.route('/get_data/<int:key>',methods=['GET'])
+def get_data(key):
+    for index, files in enumerate(os.listdir(path),1):
+        if key == index:
+            df = pd.read_csv(path+"/"+files)
+            df = data_processing(df,index)
+            json_data = df.to_json()
+            return Response(json_data,mimetype='application/json')
+        
+    return jsonify({"Error":"Invalid Server Entered!! Please Enter a value between 1 to 8 i.e. the server number"})
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+# In[68]:
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
+
+# In[ ]:
+
+
+
+
